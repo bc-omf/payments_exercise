1e8b99000ecc84da4a2e561bca296b502d179826
bc <>
2021-06-12 20:28:53 -0400

    create payment controller methods, dry up not found error for payments controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,21 @@
 class PaymentsController < ApplicationController
+  before_action :set_loan, only: %i[index show create]
+
+  def index
+    render json: @loan.payments.all
+  end
+
+  def show
+    render json: @loan.payments.find(params[:id])
+  end
+  
+  def create
+    render status: :created, json: @loan.payments.create!(payment_amount: params[:payment_amount])
+  end
+  
+  private
+  
+  def set_loan
+    @loan = Loan.find(params[:loan_id])
+  end
 end
